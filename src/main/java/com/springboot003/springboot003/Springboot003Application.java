bc5217c4cbd270daa5cbfc09a5ce8d948d4e093e
spyspy <spyhopping101@gmail.com>
2018-05-10 20:48:40 +0800

    v1

---
+++ src/main/java/com/springboot003/springboot003/Springboot003Application.java
@@ -0,0 +1,12 @@
+package com.springboot003.springboot003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot003Application.class, args);
+	}
+}
